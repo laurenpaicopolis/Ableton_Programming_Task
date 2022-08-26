63ad4db4ce08bc0bad13479af12b0e82a8d000c3
laurenpaicopolis <44977115+laurenpaicopolis@users.noreply.github.com>
2022-08-26 14:41:42 -0400

    Created `WarpMarker.cpp` and `WarpMarker.h` and completed conversion of b2s and s2b

--- WarpMarker.cpp
+++ WarpMarker.cpp
@@ -8,12 +8,18 @@
 // False is s2b
 
 double WarpMarker::calculate_conversion(double value_to_convert, std::vector<WarpMarker> &complete_structure, double end_tempo, bool conversion_type) {
-    double final_conversion = 0.0;
+    double final_conversion;
     int index = locate_warp_marker(value_to_convert, conversion_type, complete_structure);
     // Use end_time
     if (index == -1) {
         if (complete_structure.size() < 2) {
-
+            // i d k
+            if (conversion_type)
+                // b2s
+                final_conversion = value_to_convert * (1/end_tempo);
+            else
+                // s2b
+                final_conversion = value_to_convert * end_tempo;
         }
         else {
             // If end_tempo and there is more than one warp marker is used and need to do either s2b or b2s
@@ -28,32 +34,36 @@ double WarpMarker::calculate_conversion(double value_to_convert, std::vector<War
     }
     else {
         WarpMarker end_warp_marker = complete_structure.at(index);
-        WarpMarker start_warp_marker = complete_structure.at(index);
+        WarpMarker start_warp_marker = complete_structure.at(index - 1);
         double between_beat_time = end_warp_marker.beat_time - start_warp_marker.beat_time;
         double between_sample_time = end_warp_marker.sample_time - start_warp_marker.sample_time;
         // b2s
-        if (conversion_type)
-            final_conversion = value_to_convert * (between_sample_time/between_beat_time);
+        if (conversion_type) {
+            double number_to_convert = value_to_convert - start_warp_marker.beat_time;
+            final_conversion = number_to_convert * (between_sample_time/between_beat_time) + start_warp_marker.sample_time;
+        }
         // s2b
-        else
-            final_conversion = value_to_convert * (between_beat_time/between_sample_time);
+        else {
+            double number_to_convert = value_to_convert - start_warp_marker.sample_time;
+            final_conversion = number_to_convert * (between_beat_time/between_sample_time) + start_warp_marker.beat_time;
+        }
     }
     return final_conversion;
 };
 
 
-int WarpMarker:: locate_warp_marker(double time_to_convert, bool conversion_type, std::vector<WarpMarker> &complete_structure) {
+int WarpMarker::locate_warp_marker(double time_to_convert, bool time_type, std::vector<WarpMarker> &complete_structure) {
 
     if (complete_structure.size() == 1) {
         return -1;
     }
     for (int i = 0; i < complete_structure.size(); i++) {
-        if (conversion_type) {
+        if (time_type) {
             if (complete_structure.at(i).beat_time >= time_to_convert) {
                 return i;
             }
         }
-        if (!conversion_type) {
+        if (!time_type) {
             if (complete_structure.at(i).sample_time >= time_to_convert) {
                 return i;
             }
